a505b4d3581bb574fe8721ea89c71c5ff9d8dade
Marcos Anthony <marcosanthony202@gmail.com>
2024-04-06 12:34:53 -0300

    Texto LOGAR como título, tamanho da fonte e em negrito

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>LOGAR</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +14,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 40,
+    gap: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
