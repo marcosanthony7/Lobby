02ee65c323e3df1f9defe58a1db9b614424a6409
Marcos Anthony <marcosanthony202@gmail.com>
2024-04-06 12:57:30 -0300

    Logos do Google, Facebook e Apple como formas alternativas de login

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Button, Image, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
@@ -11,6 +11,26 @@ export default function App() {
       <TextInput style={styles.input} secureTextEntry={true} />
       <View style={{ width: '100%' }}>
         <Button title="Logar" color="blue" />
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 5 }}>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: 'https://t.ctcdn.com.br/mnCwSs6r1zVZvoeNtKOa9JSXs_g=/600x600/smart/i606944.png',
+          }}
+        />
+        <Image
+          style={styles.logo}
+          source={{
+            uri: 'https://t.ctcdn.com.br/DMxRsoFn2EzzWk6WaToT6sIidL8=/i489928.jpeg',
+          }}
+        />
+        <Image
+          style={styles.logo}
+          source={{
+            uri: 'https://t.ctcdn.com.br/aFp_I8ScTJJch32H29ImNebDEYU=/i489949.jpeg',
+          }}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -39,5 +59,12 @@ const styles = StyleSheet.create({
     padding: 5,
     paddingLeft: 10,
     width: '100%',
+  },
+  logo: {
+    width: 40,
+    height: 40,
+    borderWidth: 2,
+    borderColor: 'gray',
+    borderRadius: 10,
   },
 });
